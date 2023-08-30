979cdc1bc21d66c2ef17c6073fb973ae0310a8e6
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-08-30 17:01:18 +0800

    Update Bonezegei_GPS.cpp

--- src/Bonezegei_GPS.cpp
+++ src/Bonezegei_GPS.cpp
@@ -9,14 +9,65 @@
 Bonezegei_GPS::Bonezegei_GPS() {
 }
 
-Bonezegei_GPS::Bonezegei_GPS(const HardwareSerial& se) {
-  s = (HardwareSerial*)&se;
+Bonezegei_GPS::Bonezegei_GPS(HardwareSerial& serial) {
+  hardserial = (HardwareSerial*)&serial;
+  ser = 0;
 }
 
-void Bonezegei_GPS::begin(){
-  s->begin(9600);
+Bonezegei_GPS::Bonezegei_GPS(Bonezegei_SoftSerial& serial) {
+  ser = 1;
+  softserial = (Bonezegei_SoftSerial*)&serial;
 }
 
-void Bonezegei_GPS::begin(int baud){
-  s->begin(baud);
+void Bonezegei_GPS::begin() {
+  if (ser == 1) {
+    softserial->begin(9600);
+  } else {
+    hardserial->begin(9600);
+  }
+}
+
+void Bonezegei_GPS::begin(int baud) {
+  if (ser == 1) {
+    softserial->begin(baud);
+  } else {
+    hardserial->begin(baud);
+  }
+}
+
+
+uint8_t Bonezegei_GPS::getData() {
+  char ch = 0;
+  if (ser == 1) {
+    if (softserial->available()) {
+      ch = softserial->read();
+    }
+  } else {
+    if (hardserial->available()) {
+      ch = hardserial->read();
+    }
+  }
+
+  if (ch) {
+    if (ch == '$') {
+      sen_flag = 1;
+    } else if (ch == '*') {
+      sen_flag = 2;
+    }
+
+    if (sen_flag == 1) {
+      sentence[sentence_index] = ch;
+      sentence_index++;
+    } else if (sen_flag == 2) {
+
+      Serial.print("Sen:");
+      for (int a = 0; a < sentence_index; a++) {
+        Serial.write(sentence[a]);
+      }
+      Serial.println("");
+      sentence_index = 0;
+      sen_flag = 0;
+    }
+  }
+  return 0;
 }
