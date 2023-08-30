77f3d50bb3887522178240ce7e1aad4e7f977bcf
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-08-30 18:26:23 +0800

    Update Bonezegei_GPS.cpp

--- src/Bonezegei_GPS.cpp
+++ src/Bonezegei_GPS.cpp
@@ -59,15 +59,53 @@ uint8_t Bonezegei_GPS::getData() {
       sentence[sentence_index] = ch;
       sentence_index++;
     } else if (sen_flag == 2) {
-
-      Serial.print("Sen:");
-      for (int a = 0; a < sentence_index; a++) {
-        Serial.write(sentence[a]);
-      }
-      Serial.println("");
       sentence_index = 0;
       sen_flag = 0;
+      return getGPRMC();
     }
   }
   return 0;
+}
+
+
+uint8_t Bonezegei_GPS::getGPRMC() {
+  char valid=0;
+  const char s[2] = ",";
+  char* data;
+  data = strtok(sentence, s);
+  double tmp_lat = 0;
+  double tmp_lon = 0;
+
+  unsigned char index = 0;
+  if (strcmp(data, "$GPRMC") == 0) {
+    while (data != NULL) {
+      //Serial.printf("T: %s\n", data);
+
+      //Time
+      if (index == 1) {
+
+      } 
+      //validity
+      else if (index == 2) {
+        valid=data[0];
+      }  // Lattitude
+      else if (index == 3) {
+        tmp_lat = atol(data);
+        tmp_lat /= 100;
+        int lat = (int)tmp_lat;
+        latitude = lat + (((tmp_lat - lat) / 60)*100);
+      } else if (index == 4) {
+      }
+      // Longitude
+      else if (index == 5) {
+        tmp_lon = atol(data);
+        tmp_lon /= 100;
+        int lon = (int)tmp_lon;
+        longitude = lon + (((tmp_lon - lon) / 60)*100);
+      }
+      index++;
+      data = strtok(NULL, s);
+    }
+  }
+  return valid;
 }
